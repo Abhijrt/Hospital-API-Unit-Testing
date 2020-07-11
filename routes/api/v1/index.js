f6d7fd1b254b8edde38a89aca484af713a379fce
Abhay Jirati <abhi.jrt12@gmail.com>
2020-07-11 12:29:30 +0530

    adding the routes

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,5 +4,8 @@ const express = require("express");
 // taking the router form the express server
 const router = express.Router();
 
+// using the doctors route
+router.use("/doctors", require("./doctors"));
+
 // exporting the router to be used in different module or files
 module.exports = router;
