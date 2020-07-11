03cd26986bc6c91fe3e3cc5beccac38c071612f0
Abhay Jirati <abhi.jrt12@gmail.com>
2020-07-11 18:33:46 +0530

    adding the login of doctor

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -3,32 +3,33 @@ const passport = require("passport");
 // importing the jwt Strategy
 const JWTStrategy = require("passport-jwt").Strategy;
 
-// importing the extractJWt 
+// importing the extractJWt
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 
 // importing the doctor model
 const Doctor = require("../models/doctors");
 
-
 // optins for the jwtStrategy
 let opts = {
-    jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken,
-    secretOrKey : "hospital"
-}
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+  secretOrKey: "hospital",
+};
 
 // tell passport to use jwt strategy
-passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
-    Doctor.findOne(jwtPayLoad._id,function(err,user){
-        if(err){
-            console.log('Error in finding the user from JWT');
-            return;
-        }
-        if(user){
-            return done(null,user);
-        }else{
-            return done(null,false);
-        }
+passport.use(
+  new JWTStrategy(opts, function (jwtPayLoad, done) {
+    Doctor.findOne(jwtPayLoad._id, function (err, user) {
+      if (err) {
+        console.log("Error in finding the user from JWT");
+        return;
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
     });
-}));
+  })
+);
 
 module.exports = passport;
--- controllers/api/v1/doctors_controller.js
+++ controllers/api/v1/doctors_controller.js
@@ -1,4 +1,5 @@
 const Doctor = require("../../../models/doctors");
+const jwt = require("jsonwebtoken");
 
 module.exports.register = async function (req, res) {
   try {
@@ -30,4 +31,23 @@ module.exports.register = async function (req, res) {
   }
 };
 
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = async function (req, res) {
+  try {
+    let doctor = await Doctor.findOne({ name: req.body.name });
+    if (!doctor || doctor.password != req.body.password) {
+      return res.json(422, {
+        message: "Invalid username or Password",
+      });
+    }
+    return res.json(200, {
+      message: "Sign in Successfull",
+      data: {
+        token: jwt.sign(doctor.toJSON(), "hospital", { expiresIn: "10000" }),
+      },
+    });
+  } catch (err) {
+    return res.json(400, {
+      message: "Internal Server Error",
+    });
+  }
+};
